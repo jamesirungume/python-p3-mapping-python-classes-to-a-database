62e5bb590558e46edeaaa360c5bb2ffd73258e6e
Your Name <jymohcooler@gmail.com>
2023-08-28 11:53:24 +0300

    complete project

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,9 @@
 from config import CONN, CURSOR
 from song import Song
 
-
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+    song = Song.create("Hello", "25")
+    print("Song Name:", song.name)
+    print("Song Album:", song.album)
